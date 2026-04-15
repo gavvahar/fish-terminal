63c7880fb94f3e741fa03925b2397abe385b3a7f
Nihar Gavva <nihargavva@gmail.com>
2026-04-15 10:51:12 -0400

    initial fish config

---
+++ config.fish
@@ -0,0 +1,25 @@
+if status is-interactive
+    zoxide init fish | source
+    fzf --fish | source
+    function zi
+        set dir (zoxide query -l | fzf --query "$argv" --exact)
+        if test -n "$dir"
+            cd $dir
+        end
+    end
+end
+
+set -x SSH_AUTH_SOCK $HOME/.bitwarden-ssh-agent.sock
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f $HOME/miniconda3/bin/conda
+    eval $HOME/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "$HOME/miniconda3/etc/fish/conf.d/conda.fish"
+        . "$HOME/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "$HOME/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <
