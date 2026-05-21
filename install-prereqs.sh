67a2b542dbf9dad39f241da2a40adfa1bfe8087d
Nihar Gavva <nihargavva@gmail.com>
2026-05-20 21:54:03 -0400

    Add libatomic1 to prerequisite installation script

--- install-prereqs.sh
+++ install-prereqs.sh
@@ -4,5 +4,5 @@ set -e
 
 echo "Installing prerequisites..."
 sudo apt update
-sudo apt install -y unzip curl git fontconfig
+sudo apt install -y unzip curl git fontconfig libatomic1
 echo "✅ Prerequisites installed"
