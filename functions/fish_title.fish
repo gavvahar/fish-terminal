ebd93de449853e9e109992bc61d4fc3cae2f0c60
Nihar Gavva <nihargavva@gmail.com>
2026-05-29 19:42:36 -0400

    fix: update fish_title function to display different titles based on the day of the week

--- functions/fish_title.fish
+++ functions/fish_title.fish
@@ -1,3 +1,7 @@
 function fish_title
-    echo "J.A.R.V.I.S. — "(prompt_pwd)
+    if test (date "+%u") -eq 5
+        echo "F.R.I.D.A.Y. — "(prompt_pwd)
+    else
+        echo "J.A.R.V.I.S. — "(prompt_pwd)
+    end
 end
